da489c98fc8b1e62f182e2e8a206798e010e8ea0
Jimmy Vanetta <j_vanetta@yahoo.com>
2018-02-27 09:46:56 -0500

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function() 
+{
+
+ window.onscroll = function() {scrollFunction()
+};
+
+function scrollFunction() 
+{
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) 
+    {
+         $("#myBtn").fadeIn(850);
+    } 
+    else if(document.documentElement.scrollTop < 75)
+    {
+        $("#myBtn").fadeOut(850);
+    }
+} 
+
+});
+
+function topFunction() 
+{
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+
+
